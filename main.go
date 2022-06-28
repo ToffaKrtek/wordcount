f76bb70b9d29d99d1ecaeba90784000b618eb5c6
ToffaKrtek <krotov.anthon@yandex.ru>
2022-06-28 20:03:43 +0700

    ?

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 func main(){
 	sum := 0
 	len_args := len(os.Args) - 1
-	var arg string
+	if len_args > 0 {
 
-	for i:= 1; i <= len_args; i++ {
-		arg = os.Args[i]
-		arr_arg := strings.Split(arg, " ")
-		sum += len(arr_arg)
+		var arg string
+
+		for i:= 1; i <= len_args; i++ {
+			arg = os.Args[i]
+			arr_arg := strings.Split(arg, " ")
+			sum += len(arr_arg)
+		}
+		fmt.Println(sum)
 	}
-	fmt.Println(sum)
+
 }
